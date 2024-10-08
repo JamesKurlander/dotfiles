ab2a5d896312ceda67b36f91b047a5d6a80f2b62
jameskurlander-fortune <james.kurlander@fortune.com>
2024-10-08 11:20:55 -0400

    Added vim-polyglot for sytnax highlighting across all filetypes.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,44 +3,22 @@ return {
   config = function()
     local lspconfig = require("lspconfig");
 
-    -- Example: Set up a specific language server (e.g., pyright)
-    lspconfig.pyright.setup({})
-
+    lspconfig.csslss.setup({});
+    lspconfig.graphql.setup({});
+    lspconfig.html.setup({});
+    lspconfig.lua_ls.setup({});
     lspconfig.ts_ls.setup({});
     lspconfig.eslint.setup({});
+    lspconfig.phpactor.setup({
+      filetypes = { 'php', 'blade' },
+    });
+    lspconfig.tailwindcss.setup({});
 
     lspconfig.typos_lsp.setup({
       cmd_env = { RUST_LOG = "error" },
       init_options = {
         config = "~/code/typos-lsp/crates/typos-lsp/tests/typos.toml",
         diagnosticSeverity = "Hint",
-      },
-    });
-
-    lspconfig.emmet_language_server.setup({
-      filetypes = {
-        "css",
-        "eruby",
-        "html",
-        "javascript",
-        "javascriptreact",
-        "less",
-        "sass",
-        "scss",
-        "pug",
-        "typescriptreact",
-        "php",
-      },
-      init_options = {
-        includeLanguages = {},
-        excludeLanguages = {},
-        extensionsPath = {},
-        preferences = {},
-        showAbbreviationSuggestions = true,
-        showExpandedAbbreviation = "always",
-        showSuggestionsAsSnippets = false,
-        syntaxProfiles = {},
-        variables = {},
       },
     });
   end,
