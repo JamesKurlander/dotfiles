58273c9fb588780e5e97210306a4a534c1e4ce92
jameskurlander-fortune <james.kurlander@fortune.com>
2024-10-09 16:14:25 -0400

    Configured Harpoon to use Telescope interface and added some keybindings for common behaviors.

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,24 +1,59 @@
 return {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope.nvim',
+    },
     config = function()
-        require('harpoon').setup({})
-
+        local harpoon = require('harpoon')
         local mark = require('harpoon.mark')
         local ui = require('harpoon.ui')
+        local conf = require("telescope.config").values
+        local actions = require("telescope.actions")
+        local action_state = require("telescope.actions.state")
+        harpoon.setup({})
 
-        -- Keybindings
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files) do
+                table.insert(file_paths, item.filename)
+            end
+
+            require("telescope.pickers").new({}, {
+                    prompt_title = "Harpoon",
+                    prompt_prefix = ' 🔱 ',
+                    finder = require("telescope.finders").new_table({
+                            results = file_paths,
+                        }),
+                    previewer = conf.file_previewer({}),
+                    sorter = conf.generic_sorter({}),
+                    attach_mappings = function(_, map)
+                        map("i", "<C-d>", function(prompt_bufnr)
+                            local selection = action_state.get_selected_entry()
+                            if selection then
+                                require('harpoon.mark').rm_file(selection[1])
+                                actions.close(prompt_bufnr)
+                                toggle_telescope(harpoon.get_mark_config().marks)
+                            end
+                        end)
+                        return true
+                    end,
+                }):find()
+        end
+
+        -- Keybinding to toggle Telescope with Harpoon marks
+        vim.keymap.set("n", "<leader>hh", function()
+            toggle_telescope(harpoon.get_mark_config().marks)
+        end, { desc = "Open Harpoon window with Telescope" })
+
+        -- Additional Keybindings
         vim.keymap.set("n", "<leader>a", function() mark.add_file() end, { desc = "Add file to Harpoon" })
-        vim.keymap.set("n", "<C-e>", function() ui.toggle_quick_menu() end, { desc = "Toggle Harpoon menu" })
+        vim.keymap.set("n", "<leader>d", function() mark.rm_file() end, { desc = "Remove file from Harpoon" })
+        vim.keymap.set("n", "<leader>hd", function() mark.clear_all() end, { desc = "Clear all files from Harpoon" })
 
-        vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end, { desc = "Navigate to Harpoon file 1" })
-        vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end, { desc = "Navigate to Harpoon file 2" })
-        vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end, { desc = "Navigate to Harpoon file 3" })
-        vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end, { desc = "Navigate to Harpoon file 4" })
-
-        -- Toggle previous & next buffers stored within Harpoon list
-        vim.keymap.set("n", "<C-S-P>", function() ui.nav_prev() end, { desc = "Navigate to previous Harpoon file" })
-        vim.keymap.set("n", "<C-S-N>", function() ui.nav_next() end, { desc = "Navigate to next Harpoon file" })
+        -- Navigate previous & next buffers in Harpoon
+        vim.keymap.set("n", "<C-h>", function() ui.nav_prev() end, { desc = "Navigate to previous Harpoon file" })
+        vim.keymap.set("n", "<C-l>", function() ui.nav_next() end, { desc = "Navigate to next Harpoon file" })
     end
 }
