ddbe2cbc831c599d33ec655f137cc3e71631c38c
jameskurlander-fortune <james.kurlander@fortune.com>
2024-10-10 11:37:57 -0400

    Modified some ChatGPT settings.

--- nvim/lua/plugins/chatgpt.lua
+++ nvim/lua/plugins/chatgpt.lua
@@ -9,10 +9,25 @@ return {
     },
     config = function()
         require("chatgpt").setup({
-                api_key_cmd = "echo $OPENAI_API_KEY",
-            })
+            api_key_cmd = "echo $OPENAI_API_KEY",
+            openai_params = {
+                model = 'chatgpt-4o-latest',
+            },
+            chat = {
+                welcome_message = 'Ask me anything...'
+            },
+            popup_layout = {
+                default = 'right',
+            },
+            popup_window = {
+                border = {
+                    text = {
+                        top = " ChatGPT ",
+                    },
+                },
+            },
+        })
 
-
-        vim.keymap.set("n", "<leader>cc", ':ChatGPT<CR>', { noremap = true, silent = true })
-    end
+    vim.keymap.set("n", "<leader>cc", ':ChatGPT<CR>', { noremap = true, silent = true })
+end
 }
