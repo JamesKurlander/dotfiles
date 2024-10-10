b30d960d04ab2a2e2d30f5a73054d0af524cdb65
jameskurlander-fortune <james.kurlander@fortune.com>
2024-10-10 16:16:27 -0400

    Added scss LSP.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,22 +4,23 @@ return {
     local lspconfig = require("lspconfig");
 
     lspconfig.cssls.setup({});
+    lspconfig.somesass_ls.setup({});
     lspconfig.graphql.setup({});
     lspconfig.html.setup({});
     lspconfig.lua_ls.setup({});
     lspconfig.ts_ls.setup({});
     lspconfig.eslint.setup({});
     lspconfig.phpactor.setup({
-      filetypes = { 'php', 'blade' },
-    });
+        filetypes = { 'php', 'blade' },
+      });
     lspconfig.tailwindcss.setup({});
 
     lspconfig.typos_lsp.setup({
-      cmd_env = { RUST_LOG = "error" },
-      init_options = {
-        config = "~/code/typos-lsp/crates/typos-lsp/tests/typos.toml",
-        diagnosticSeverity = "Hint",
-      },
-    });
+        cmd_env = { RUST_LOG = "error" },
+        init_options = {
+          config = "~/code/typos-lsp/crates/typos-lsp/tests/typos.toml",
+          diagnosticSeverity = "Hint",
+        },
+      });
   end,
 }
