f113a9fde13dc1aa4b646da1dc738380b7d923ae
JamesKurlander <james.h.kurlander@gmail.com>
2024-10-20 14:15:31 -0400

    Cleaed up noice config.

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,20 +1,12 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  opts = {
-    -- add any options here
-  },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     'MunifTanjim/nui.nvim',
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     'rcarriga/nvim-notify',
   },
   config = function()
     require('notify').setup({
-      --stages = 'static',
       render = 'compact',
       fps = 120,
     });
@@ -23,13 +15,13 @@ return {
       cmdline = {
         format = {
           cmdline = {
-            title = "  COMMAND  ",  -- Set a custom title for the command line popup
+            title = "  COMMAND  ",
           },
           search_down = {
-            title = "  Search 󰁃  ", -- Custom title for search (down)
+            title = "  Search 󰁃  ",
           },
           search_up = {
-            title = "  Search 󰁜  ", -- Custom title for search (up)
+            title = "  Search 󰁜  ",
           },
         },
       },
@@ -46,16 +38,15 @@ return {
         override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
           ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+          ['cmp.entry.get_documentation'] = true,
         },
       },
-      -- you can enable a preset for easier configuration
       presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = false, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
+        bottom_search = false,
+        command_palette = false,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
       },
     });
   end
