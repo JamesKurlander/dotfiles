d230a25a2805c665191a5b6ded42ccc7d724ad14
JamesKurlander <james.h.kurlander@gmail.com>
2024-10-28 21:32:10 -0400

    Fixed ncspot artist display on tmux status line.

--- scripts/ncspot_current_song.sh
+++ scripts/ncspot_current_song.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-song=$(nc -U /tmp/ncspot-501/ncspot.sock | jq -r '.playable.title + " by " + (.playable.album_artists[0])')
+song=$(nc -U /tmp/ncspot-501/ncspot.sock | jq -r '.playable.title + " by " + (.playable.artists[0])')
 if [ -n "$song" ]; then
   echo "🎧 $song"
 fi
