d42e1852a72ffa3fa040fa094f0234f4dfb22b4b
JamesKurlander <james.h.kurlander@gmail.com>
2024-10-28 21:21:41 -0400

    Fixed hidden files not appearing in Telescope.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,33 +3,43 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     require('telescope').setup({
-        defaults = {
-          vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-            "--hidden",
-            "--glob", "!.git/*"
-          },
-          find_command = {
-            'rg',
-            '--files',
-            '--hidden',
-            '--glob'
-          },
-          prompt_prefix = ' 🔭 ',
-          selection_caret = ' ',
-          mappings = {
-            i = {
-              ['<C-j>'] = require('telescope.actions').move_selection_next,
-              ['<C-k>'] = require('telescope.actions').move_selection_previous,
-            },
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--hidden",
+          "--glob", "!**/.git/*"
+        },
+        find_command = {
+          'rg',
+          '--files',
+          '--hidden',
+          '--glob', '!**/.git/*'
+        },
+        prompt_prefix = ' 🔭 ',
+        selection_caret = ' ',
+        mappings = {
+          i = {
+            ['<C-j>'] = require('telescope.actions').move_selection_next,
+            ['<C-k>'] = require('telescope.actions').move_selection_previous,
           },
         },
-      });
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+        live_grep = {
+          additional_args = function()
+            return { "--hidden" }
+          end,
+        },
+      },
+    });
   end,
 }
