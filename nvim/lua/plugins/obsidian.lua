d75e3118ce8b3ca00dbd5ec5f4bee1a2a0e367c9
jameskurlander-fortune <james.kurlander@fortune.com>
2024-11-07 16:39:08 -0500

    Fixed note_id_func.

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -8,6 +8,17 @@ return {
   },
   opts = {
     disable_frontmatter = true,
+    note_id_func = function(title)
+      local suffix = ""
+      if title ~= nil then
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end,
     workspaces = {
       {
         name = "work",
