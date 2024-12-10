1b529a9340c78ddf02c3daa447272b8bd2d8c353
jameskurlander-fortune <james.kurlander@fortune.com>
2024-12-10 15:05:03 -0500

    Cleaned up setup script.

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,4 +1,4 @@
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
 
-defaults write com.apple.Dock autohide-delay -float 5; killall Dock
+# defaults write com.apple.Dock autohide-delay -float 5; killall Dock
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -27,13 +27,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install Homebrew packages
 brew tap homebrew/bundle
-
-if [[ -f "$DOTFILES_DIR/homebrew/Brewfile.work" ]]; then
-  echo "Installing from Brewfile.work..."
-  brew bundle --file="$DOTFILES_DIR/homebrew/Brewfile.work"
-else
-  echo "Brewfile.work not found in dotfiles directory."
-fi
+brew bundle --file="$DOTFILES_DIR/homebrew/Brewfile.work"
 
 if [[ "$personal_flag" == true && -f "$DOTFILES_DIR/homebrew/Brewfile.personal" ]]; then
   echo "Installing from Brewfile.personal..."
@@ -43,39 +37,28 @@ else
 fi
 
 # Symlink for .zshrc
-if [[ -f "$HOME/.zshrc" || -L "$HOME/.zshrc" ]]; then
-  echo "Backing up existing .zshrc to .zshrc.bak"
-  mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
-fi
 ln -sf "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
 echo "Symlinked .zshrc"
 
 # Symlink for Neovim
-if [[ -d "$HOME/.config/nvim" || -L "$HOME/.config/nvim" ]]; then
-  echo "Backing up existing nvim config to nvim_backup"
-  mv "$HOME/.config/nvim" "$HOME/.config/nvim_backup"
-fi
 ln -sf "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
 echo "Symlinked Neovim config"
 
 # Symlink for tmux.conf
-if [[ -f "$HOME/.tmux.conf" || -L "$HOME/.tmux.conf" ]]; then
-    echo "Backing up existing .tmux.conf to .tmux.conf.bak"
-    mv "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak"
-fi
 ln -sf "$DOTFILES_DIR/tmux/tmux.conf" "$HOME/.tmux.conf"
 echo "Symlinked .tmux.conf"
 
 # Symlink for spotify-player
-if [[ -f "$HOME/.config/spotify-player/app.toml" || -L "$HOME/.config/spotify-player/app.toml" ]]; then
-    echo "Backing up existing /spotify-player/app.toml to /spotify-player/app.toml"
-    mv "$HOME/.config/spotify-player/app.toml" "$HOME/.config/spotify-player/app.toml.bak"
-fi
 ln -sf "$DOTFILES_DIR/spotify-player/app.toml" "$HOME/.config/spotify-player/app.toml"
 echo "Symlinked spotify-player/app.toml"
 
 # Symlink for yazi
 ln -sf "$DOTFILES_DIR/yazi" "$HOME/.config/yazi"
+echo "Symlinked yazi"
+
+# Symlink for newsboat
+ln -sf "$DOTFILES_DIR/newsboat/urls" "$HOME/.newsboat/urls"
+echo "Symlinked newsboat"
 
 # Refresh the terminal
 #reset
